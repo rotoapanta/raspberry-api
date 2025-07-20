db7a8daea19b9ee0be4a7676ea2e2dda6fe864bf
Roberto Toapanta <robertocarlos.toapanta@gmail.com>
2025-07-19 20:27:02 -0500

    upodate DEPLOY.md

--- app/core/config.py
+++ app/core/config.py
@@ -8,8 +8,6 @@ from dotenv import load_dotenv
 # Load variables from .env if it exists
 load_dotenv()
 
-BACKEND_IP = os.getenv("BACKEND_IP", "192.168.1.100")
-BACKEND_PORT = os.getenv("BACKEND_PORT", "8001")
 REGISTER_INTERVAL = int(os.getenv("REGISTER_INTERVAL", 60))
 LOG_INTERVAL = int(os.getenv("LOG_INTERVAL", 10))
 LOG_DIR = os.getenv("LOG_DIR", "logs")
